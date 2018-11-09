adcf83b79f21e097ec522d11698c10468f54b7f8
FF_Adrian <adrian@firstfactory.com>
2018-11-09 16:48:13 -0600

    lesson 130 3min

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/_Aux'
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
         return <li key={igKey}>
